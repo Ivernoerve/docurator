aba8cf7df0de5944d11c586753126dd1f82a5b69
Ivernoerve-mbp14 <ino033@uit.no>
2024-09-28 13:20:04 +0200

    implement new spec for doc containers

--- docurator/docurator/doc_containers.py
+++ docurator/docurator/doc_containers.py
@@ -1,13 +1,44 @@
 """Conatins container dataclasses to store the different documentations. """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Type
 
-from inspect import signature
+import inspect
 
 
 @dataclass(frozen=True)
 class Docs:
+    """Represents documentation information.
+
+    Attributes:
+        name (str): The name of the object.
+        docstring (Optional[str]): The docstring of the object, or None if no docstring is provided.
+    """
+    name: str
+    docstring: str|None
+    
+
+@dataclass(frozen=True)
+class ModuleDocs:
+    """Represents documentation information for a module.
+
+    Attributes:
+        name (str): The name of the object.
+        docstring (str|None): The docstring of the object, or None if no docstring is provided.
+        contents list[Docs]: A list of documentation objects.
+    """
+    name: str
+    docstring: str|None
+    __contents: list[Docs] = field(default_factory=list)
+    
+    @property
+    def contents(self) -> list[Docs]:
+        """Interact with the __object attribute"""
+        return self.__contents
+
+
+@dataclass(frozen=True)
+class ObjectDocs(Docs):
     """Represents documentation information for an object.
 
     Attributes:
@@ -18,21 +49,28 @@ class Docs:
         f_signature (Type[signature]): The function signature, if applicable.
         module (Optional[str]): The name of the module where the object is defined, or None if not applicable.
     """
-    docstring: str|None
-    name: str
     qualname: str
     type: str
-    f_signature: Type[signature]
-    module: str|None = None
+    f_signature: Type[inspect.signature]
 
-class ClassDocs(Docs):
+
+class ClassDocs(ObjectDocs):
     """Represents documentation information for a class, including its parents.
 
     Inherits from:
         Docs: The base class containing common documentation attributes.
 
     Attributes:
+        name (str): The name of the object.
+        docstring (str|None): The docstring of the object, or None if no docstring is provided.
         parents (List[object]): A list of parent classes of the documented class.
+        contents list[Docs]: A list of documentation objects.
     """
     parents: list[object]
+    __contents: list[Docs] = field(default_factory=list)
+    
+    @property
+    def contents(self) -> list[Docs]:
+        """Interact with the __object attribute"""
+        return self.__contents
 
